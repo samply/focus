3d5c085cedf0327f2b03d7786458f459eb5f1c31
Enola Knezevic <enola.knezevic@dkfz-heidelberg.com>
2023-12-22 10:15:52 +0100

    beautified

--- src/exporter.rs
+++ src/exporter.rs
@@ -26,57 +26,54 @@ const EXECUTE: Params = Params {
 };
 
 pub async fn post_exporter_query(body: &String, execute: bool) -> Result<String, FocusError> {
-    match &CONFIG.exporter_url {
-        None => Err(FocusError::MissingExporterEndpoint()),
-        Some(exporter_url) => {
-            let exporter_params = if execute { EXECUTE } else { CREATE };
-            debug!("{} exporter query...", exporter_params.doing);
+    let Some(exporter_url) = &CONFIG.exporter_url else {
+        return Err(FocusError::MissingExporterEndpoint());
+    };
 
-            let mut headers = HeaderMap::new();
+    let exporter_params = if execute { EXECUTE } else { CREATE };
+    debug!("{} exporter query...", exporter_params.doing);
 
-            headers.insert(
-                header::CONTENT_TYPE, //TODO discard the result, just return OK
-                HeaderValue::from_static("text/html; charset=UTF-8"),
-            );
+    let mut headers = HeaderMap::new();
 
-            if let Some(auth_header_value) = CONFIG.auth_header.clone() {
-                headers.insert(
-                    header::AUTHORIZATION,
-                    HeaderValue::from_str(auth_header_value.as_str())
-                        .map_err(FocusError::InvalidHeaderValue)?,
-                );
-            }
+    headers.insert(
+        header::CONTENT_TYPE, //TODO discard the result, just return OK
+        HeaderValue::from_static("text/html; charset=UTF-8"),
+    );
 
-            let resp = CONFIG
-                .client
-                .post(format!(
-                    "{}{}",
-                    exporter_url,
-                    exporter_params.method
-                ))
-                .headers(headers)
-                .body(body.clone())
-                .send()
-                .await
-                .map_err(FocusError::UnableToPostExporterQuery)?;
-
-            debug!("{} query...", exporter_params.done);
-
-            let text = match resp.status() {
-                StatusCode::OK => {
-                    format!("Query successfully {}", exporter_params.done)
-                }
-                code => {
-                    warn!("Got unexpected code {code} while {} query; reply was `{}`, debug info: {:?}", exporter_params.doing, body, resp);
-                    return Err(FocusError::ExporterQueryErrorReqwest(format!(
-                        "Error while {} query: {:?}",
-                        exporter_params.doing,
-                        resp
-                    )));
-                }
-            };
-
-            Ok(text)
-        }
+    if let Some(auth_header_value) = CONFIG.auth_header.clone() {
+        headers.insert(
+            header::AUTHORIZATION,
+            HeaderValue::from_str(auth_header_value.as_str())
+                .map_err(FocusError::InvalidHeaderValue)?,
+        );
     }
+
+    let resp = CONFIG
+        .client
+        .post(format!("{}{}", exporter_url, exporter_params.method))
+        .headers(headers)
+        .body(body.clone())
+        .send()
+        .await
+        .map_err(FocusError::UnableToPostExporterQuery)?;
+
+    debug!("{} query...", exporter_params.done);
+
+    let text = match resp.status() {
+        StatusCode::OK => {
+            format!("Query successfully {}", exporter_params.done)
+        }
+        code => {
+            warn!(
+                "Got unexpected code {code} while {} query; reply was `{}`, debug info: {:?}",
+                exporter_params.doing, body, resp
+            );
+            return Err(FocusError::ExporterQueryErrorReqwest(format!(
+                "Error while {} query: {:?}",
+                exporter_params.doing, resp
+            )));
+        }
+    };
+
+    Ok(text)
 }
