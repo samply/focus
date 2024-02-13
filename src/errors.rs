c8c23592143a4f3977110e637b6196114ca727b5
Enola Knezevic <115070135+enola-dkfz@users.noreply.github.com>
2024-02-13 14:33:55 +0100

    Update src/errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -50,7 +50,7 @@ pub enum FocusError {
 }
 
 impl FocusError {
-    /// Generate a descriptive error message that does not leak any sensitive data that might me contained inside the error value
+    /// Generate a descriptive error message that does not leak any sensitive data that might be contained inside the error value
     pub fn user_facing_error(&self) -> &'static str {
         use FocusError::*;
         // TODO: Add more match arms
