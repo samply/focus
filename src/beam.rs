f1eabc31028c64ecb4eafb1fc9838de7ac26bc1a
janskiba <jan.skiba@dkfz-heidelberg.de>
2024-02-13 12:46:34 +0000

    feat: Cancel claiming task

--- src/beam.rs
+++ src/beam.rs
@@ -85,9 +85,9 @@ pub async fn retrieve_tasks() -> Result<Vec<TaskRequest<String>>, FocusError> {
         .map_err(FocusError::UnableToRetrieveTasksHttp)
 }
 
-pub async fn answer_task<T: Serialize + 'static>(task_id: MsgId, result: &TaskResult<T>) -> Result<bool, FocusError> {
-    debug!("Answer task with id: {task_id}");
-    BEAM_CLIENT.put_result(result, &task_id)
+pub async fn answer_task<T: Serialize + 'static>(result: &TaskResult<T>) -> Result<bool, FocusError> {
+    debug!("Answer task with id: {}", result.task);
+    BEAM_CLIENT.put_result(result, &result.task)
         .await
         .map_err(FocusError::UnableToAnswerTask)
 }
