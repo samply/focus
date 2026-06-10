c10c0d51cba83fa44671eb970734f9420249def9
Enola Knezevic <enola.knezevic@dkfz-heidelberg.de>
2026-06-10 11:46:30 +0200

    formetting

--- src/blaze.rs
+++ src/blaze.rs
@@ -33,7 +33,12 @@ pub async fn check_availability() -> bool {
 
     let resp = match CONFIG
         .client
-        .get(format!("{}health", (CONFIG.endpoint_url.to_string().split("fhir")).next().expect("Missing \"fhir\" in Blaze url base")))
+        .get(format!(
+            "{}health",
+            (CONFIG.endpoint_url.to_string().split("fhir"))
+                .next()
+                .expect("Missing \"fhir\" in Blaze url base")
+        ))
         .send()
         .await
     {
