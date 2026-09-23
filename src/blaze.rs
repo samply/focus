b7847fc69f271e3c5fc6224620229951a1f5f6a6
Enola Knezevic <enola.knezevic@dkfz-heidelberg.de>
2026-09-23 11:55:54 +0200

    refactoring

--- src/blaze.rs
+++ src/blaze.rs
@@ -31,15 +31,17 @@ pub struct AstQuery {
 pub async fn check_availability() -> bool {
     debug!("Checking Blaze availability...");
 
-    let mut health_url = CONFIG.endpoint_url.clone();
-    let base_path = health_url
-        .path()
-        .rsplit_once("fhir")
-        .map(|(before, _)| before.to_string())
-        .expect("Missing \"fhir\" in Blaze url path");
-    health_url.set_path(&format!("{base_path}health"));
-
-    let resp = match CONFIG.client.get(health_url).send().await {
+    let resp = match CONFIG
+        .client
+        .get(format!(
+            "{}health",
+            (CONFIG.endpoint_url.to_string().rsplit_once("fhir"))
+                .expect("Missing \"fhir\" in Blaze url base")
+                .0
+        ))
+        .send()
+        .await
+    {
         Ok(response) => response,
         Err(e) => {
             warn!("Error making Blaze request: {:?}", e);
